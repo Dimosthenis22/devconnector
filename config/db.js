45905207719b251987bc867e50e28f9ab21043be
Dimosthenis22 <dimosthenisk80@gmail.com>
2023-01-31 15:05:30 +0100

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  // The next line is there so I will skip a warning
+  mongoose.set('strictQuery', true);
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      //   useCreateIndex: true,
+      //   useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
